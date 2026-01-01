ca53f037f565f2095f59b03c9c3d0671b7508925
Arpan sharma <sharmaarpan143@gmail.com>
2026-01-01 22:59:59 +0530

    axios setup

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,24 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/backend/:path*',
+        destination: 'https://dev.tabendihealthcarenetwork.com/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
---
+++ src/lib/axios.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+
+const api = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_PROXY_API_URL, // your backend URL
+    withCredentials: true, // allows sending cookies if backend uses them
+});
+
+
+api.interceptors.request.use(
+    (config) => {
+
+        return config;
+    },
+    (error) => {
+
+        Promise.reject(error)
+    }
+);
+
+// Response interceptor → handle errors
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+
+        return Promise.reject(error);
+    }
+);
+
+export default api;
+
